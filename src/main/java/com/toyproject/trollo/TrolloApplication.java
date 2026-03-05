f62787462b43f2b21768f20a44267dec29641270
youngseok <gys329@naver.com>
2026-03-05 16:28:09 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/com/toyproject/trollo/TrolloApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.trollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrolloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproject/trollo/TrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.trollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrolloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
